5022ec624194356170f128238ff5d0cb87b01622
Surma <surma@surma.link>
2015-10-10 15:44:49 +0100

    Making module loading work on slow connections

--- app/modules/main.js
+++
@@ -1,7 +0,0 @@
-import {default as Moment} from '../bower_components/moment/moment';
-import {birthday} from 'birthday';
-
-document.querySelector('#birthday').textContent = Moment(birthday).fromNow();
-
-import {default as cssLoader} from 'defer-css';
-cssLoader();
