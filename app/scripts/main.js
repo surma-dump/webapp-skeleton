8a4dab651615c22432f5c8d86f0387523978e043
Surma <surma@surma.link>
2015-10-10 15:31:18 +0100

    Idiomatize JS setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
-import {default as Moment} from '../bower_components/moment/moment';
-import {birthday} from 'birthday';
+import {default as Moment} from 'bower_components/moment/moment';
+import {birthday} from 'modules/birthday';
 
 document.querySelector('#birthday').textContent = Moment(birthday).fromNow();
 
-import {default as cssLoader} from 'defer-css';
+import {default as cssLoader} from 'modules/defer-css';
 cssLoader();
