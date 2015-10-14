04f7516d34bbc59d819f7a92ac414ce3bd19ba9e
Surma <surma@surma.link>
2015-10-14 14:33:23 +0100

    Well, I'm an idiot

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -53,7 +53,7 @@ Object.keys(pipelines).forEach(extension => {
     var stream = gulp.src([
       'app/**/*.' + extension
     ]);
-    pipeline.reduce((stream, step) => stream.pipe(step), stream);
+    stream = pipeline.reduce((stream, step) => stream.pipe(step), stream);
     return stream.pipe(gulp.dest('dist'));
   });
 });
