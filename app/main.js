dbf663f92d4ec93ed8095bbd7388769af33c614e
Surma <surma@surma.link>
2015-10-20 23:10:17 +0100

    Remove uncessarily verbose imports

--- app/main.js
+++ app/main.js
@@ -1,8 +1,8 @@
-import {default as Moment} from 'bower_components/moment/min/moment.min';
-import {default as importantDate} from 'modules/important-date';
+import Moment from 'bower_components/moment/min/moment.min';
+import importantDate from 'modules/important-date';
 
 document.querySelector('#birthday').textContent =
   Moment(importantDate).fromNow();
 
-import {default as cssLoader} from 'modules/defer-css';
+import cssLoader from 'modules/defer-css';
 cssLoader();
